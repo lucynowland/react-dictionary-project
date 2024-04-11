e82c64c78eed83d6c7f8e154164fd8538e106b0f
Lucy Nowland <lucynowland03@gmail.com>
2024-04-11 10:38:28 +0100

    Add footer html & styling to App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="heading">Dictionary</header>
+      <div className="container">
+        <header className="heading">Dictionary</header>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://github.com/lucynowland"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Lucy Nowland
+          </a>{" "}
+          which is open-sourced on{" "}
+          <a
+            href="https://github.com/lucynowland/react-dictionary-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          & hosted on{" "}
+          <a
+            href="https://react-dictionary-search-project.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
