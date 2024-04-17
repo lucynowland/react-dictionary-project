298f55b3b0d45ffbc7b1030524a6130ad35ac25a
Lucy Nowland <lucynowland03@gmail.com>
2024-04-17 11:47:31 +0100

    Loop through & display synonyms

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,15 +1,36 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 import "./SearchResults.css";
 
 export default function Meanings(props) {
   console.log(props.meanings);
-  return (
-    <section className="MeaningsSection">
-      <div className="Meanings">
-        <h3>{props.meanings.partOfSpeech}</h3>
-        <p>{props.meanings.definition}</p>
-        <em>{props.meanings.example}</em>
-      </div>
-    </section>
-  );
+
+  if (props.meanings.example && props.meanings.synonyms) {
+    return (
+      <section className="MeaningsSection">
+        <div className="Meanings">
+          <h3>{props.meanings.partOfSpeech}</h3>
+          <p>
+            <strong>Definition:</strong> {props.meanings.definition}
+          </p>
+          <em>
+            {" "}
+            <strong>Exemplar:</strong> {props.meanings.example}
+          </em>
+          <Synonyms synonyms={props.meanings.synonyms} />
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section className="MeaningsSection">
+        <div className="Meanings">
+          <h3>{props.meanings.partOfSpeech}</h3>
+          <p>
+            <strong>Definition:</strong> {props.meanings.definition}
+          </p>
+        </div>
+      </section>
+    );
+  }
 }
